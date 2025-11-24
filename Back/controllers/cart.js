8ac0503c9d419059bc15633d9cfb295a8a6df346
Rosa Lizeth <rosysuarezmedina2005@gmail.com>
2025-11-23 22:15:40 -0600

    Agregando modelo de cupones y modificando controladores items y cart

--- Back/controllers/cart.js
+++ Back/controllers/cart.js
@@ -1,41 +1,192 @@
+const {
+  getCart,
+  addToCart,
+  updateCartItem,
+  deleteCartItem,
+  applyCoupon
+} = require('../models/cart');
+
+const {
+  addToCart,
+  updateCartItem,
+  deleteCartItem,
+} = require('../models/products');
+
+const {
+  applyCoupon
+} = require('../models/coupons');
+
 // Carrito
 const getCart = async (req, res) => {
   try {
-    // Obtener el carrito del usuario
+    const userId = req.user.id; // Representa al usuario para poder guardar su carrito
+    if(!userId){
+      return res.status(400).json({
+        success: false,
+        message: 'Usuario inválido.'
+      });
+    }
+
+    const verCarrito = await cart.getCart(); // Función que está en models/cart
+    res.json(verCarrito);
+
   } catch (error) {
-    // Manejo de errores
+    console.error('Error en ver carrito:', error);
+    return res.status(500).json({
+      success: false,
+      message: 'Error en el servidor'
+    });
   }
 };
 
 const addToCart = async (req, res) => {
   try {
-    // Agregar producto al carrito
+    const { productID, quantity } = req.body;
+    const userId = req.user.id;
+    
+    // Validaciones
+    if(!productID || !quantity) {
+      return res.status(400).json({
+        success: false,
+        message: "Ingresar ID y cantidad"
+      });
+    }
+
+    if(quantity <= 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Cantidad debe ser mayor a 0."
+      });
+    }
+
+    const exists = await products.getProductId(productID);
+    if(!exists){
+      return res.status(404).json({
+        success: false,
+        message: "Producto no existente."
+      });
+    }
+
+    const add = await cart.addCart(userId, productID, quantity);
+    res.json(add);
+
   } catch (error) {
-    // Manejo de errores
+    console.error('Error al agregar al carrito:', error);
+    return res.status(500).json({
+      success: false,
+      message: 'Error en el servidor'
+    });
   }
 };
 
 const updateCartItem = async (req, res) => {
   try {
-    // Modificar cantidad de producto en el carrito
+    const { productID } = req.params;
+    const { quantity } = req.body;
+    const userId = req.user.id;
+
+    // Validaciones
+    if(!productID || !quantity) {
+      return res.status(400).json({
+        success: false,
+        message: "Ingresar ID y cantidad"
+      });
+    }
+
+    const exists = await products.getProductId(productID);
+    if(!exists){
+      return res.status(404).json({
+        success: false,
+        message: "Producto no existente."
+      });
+    }
+
+    const update = await cart.updateCart(userId, productID, quantity);
+    res.json;
+
   } catch (error) {
-    // Manejo de errores
+    console.error('Error al agregar al carrito:', error);
+    return res.status(500).json({
+      success: false,
+      message: 'Error en el servidor'
+    });
   }
 };
 
 const deleteCartItem = async (req, res) => {
   try {
-    // Eliminar producto del carrito
+    const { productID } = req.params;
+    const userId = req.user.id;
+
+    // Validaciones
+    if(!prodId){
+      return res.status(400).json({
+        success: false,
+        message: "Ingresar un ID"
+      });
+    }
+
+    const exists = await products.getProductId(productID);
+    if(!exists){
+      return res.status(404).json({
+        success: false,
+        message: "El producto no está en el carrito."
+      });
+    }
+
+    const eliminado = await cart.deletProduct(userId, productID);
+    res.json(eliminado);
+
   } catch (error) {
-    // Manejo de errores
+    console.error('Error al eliminar del carrito:', error);
+    return res.status(500).json({
+      success: false,
+      message: 'Error en el servidor'
+    });
   }
 };
 
 const applyCoupon = async (req, res) => {
   try {
-    // Aplicar cupón al carrito
+    const { coupon } = req.body;
+    const userId = req.user.id;
+
+    // Validaciones
+    if(!coupon){
+      return res.status(400).json({
+        success: false,
+        message: "Ingresa el cupón."
+      });
+    }
+
+    const couponData = await coupons.getCoupon(coupon);
+
+    if (!couponData) {
+      return res.status(404).json({
+        success: false,
+        message: "Cupón inválido o inexistente."
+      });
+    }
+
+     /*
+      Aún falta decidir si el descuento se aplicará:
+      - Directamente al carrito
+      o
+      - Hasta el checkout.
+
+      Por ahora solo validamos el cupón y lo mandamos al modelo
+      para que lo registre.
+    */
+
+    const cupon = await cart.aplicarCupon(userId, coupon);
+    res.json(cupon);
+
   } catch (error) {
-    // Manejo de errores
+    console.error('Error al aplicar cupón', error);
+    return res.status(500).json({
+      success: false,
+      message: 'Error en el servidor'
+    });
   }
 };
 
--- Back/controllers/items.js
+++ Back/controllers/items.js
@@ -1,27 +1,84 @@
+const {
+  getAllProducts,
+  getProductsByCategory,
+  getProductById
+} = require('../models/products');
+
 // Productos
 const getAllProducts = async (req, res) => {
   try {
-    // Listar todos los productos
+    const allProducts = await products.getProducts(); // Función que está en models/products
+    res.json(allProducts);
   } catch (error) {
-    // Manejo de errores
+    console.error('Error en mostrar todos los productos:', error);
+    return res.status(500).json({
+      success: false,
+      message: 'Error en el servidor'
+    });
   }
 };
 
-const getProductsByCategory = async (req, res) => {
-  try {
-    // Listar productos por categoría
-  } catch (error) {
-    // Manejo de errores
-  }
-};
+  const getProductsByCategory = async (req, res) => {
+    try {
+      const { categoria } = req.params;
 
-const getProductById = async (req, res) => {
-  try {
-    // Obtener producto por ID
-  } catch (error) {
-    // Manejo de errores
-  }
-};
+      // Validación de categoría 
+      if(!categoria){
+        return res.status(400).json({
+          success: false,
+          message: "Ingresa la categoria valida."
+        });
+      }
+
+      const product = await products.getProductCategory(categoria);
+
+      if(!product){
+        return res.status(404).json({
+          success: false,
+          message: "Producto no encontrado"
+        });
+      }
+      res.json(product);
+
+    } catch (error) {
+      console.error('Error al encontrar producto por categoria:', error);
+      return res.status(500).json({
+        success: false,
+        message: 'Error en el servidor'
+      });
+    }
+  };
+
+  const getProductById = async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      // Validación de id
+      if(!id){
+        return res.status(400).json({
+          success: false,
+          message: "Ingresa un ID valido."
+        });
+      }
+
+      const product = await products.getProductId(id); // Función que está en models/products
+
+      if(!product){
+        return res.status(404).json({
+          success: false,
+          message: "Producto no encontrado"
+        });
+      }
+      res.json(product);
+
+    } catch (error) {
+      console.error('Error al encontrar producto por id:', error);
+      return res.status(500).json({
+        success: false,
+        message: 'Error en el servidor'
+      });
+    }
+  };
 
 module.exports = {
   getAllProducts,
