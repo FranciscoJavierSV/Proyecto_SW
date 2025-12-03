c8c315e30ff565e7be5ef8e9cdbad919281563c3
FranciscoJavierSV <deriker.denoter@gmail.com>
2025-12-02 21:01:12 -0600

    Actualizacion de rutas

--- Back/index.js
+++ Back/index.js
@@ -16,7 +16,7 @@ app.use(cors({
     "http://127.0.0.1:5500",
     "http://localhost:5500"
   ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
